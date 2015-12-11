454bce3bb577abb475f0fb829164cc70ac574e3b
rcuevasuskar <rcuevas.uskar@gmail.com>
2015-12-11 11:52:14 +0100

    make it

--- index.js
+++ index.js
@@ -2,15 +2,30 @@
 
 var rest = require('restler');
 var express=require('express');
+var http = require('http');
+var fs = require('fs');
+var index = fs.readFileSync('index.html');
 var app = express();
 var contadores = new Array;
-var puerto = 8080;
+var puerto = 5000;
 
 app.use(function(req, res, next) {
   res.header("Access-Control-Allow-Origin", "*");
   res.header("Access-Control-Allow-Headers", "Origin, X-Requested-With, Content-Type, Accept");
   res.header("Access-Control-Allow-Methods", "POST, GET, PUT")
   next();
+});
+
+app.set('port', (process.env.PORT || p));
+
+app.use(express.static(__dirname + '/public'));
+
+// views is directory for all template files
+app.set('views', __dirname + '/views');
+app.set('view engine', 'ejs');
+
+app.get('/', function(req,res) {
+  res.end(index);
 });
 
 app.put('/crear/:id', function( req,res ) {
