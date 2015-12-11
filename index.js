3445745fb4ca3bcc26db4a5c492eebbf285392fc
rcuevasuskar <rcuevas.uskar@gmail.com>
2015-12-11 10:42:54 +0100

    javascript

--- index.js
+++ index.js
@@ -1,20 +1,36 @@
-var express = require('express');
+#!/usr/bin/env
+
+var rest = require('restler');
+var express=require('express');
 var app = express();
+var contadores = new Array;
+var puerto = 8080;
 
-app.set('port', (process.env.PORT || 5000));
-
-app.use(express.static(__dirname + '/public'));
-
-// views is directory for all template files
-app.set('views', __dirname + '/views');
-app.set('view engine', 'ejs');
-
-app.get('/', function(request, response) {
-  response.render('pages/index');
+app.use(function(req, res, next) {
+  res.header("Access-Control-Allow-Origin", "*");
+  res.header("Access-Control-Allow-Headers", "Origin, X-Requested-With, Content-Type, Accept");
+  res.header("Access-Control-Allow-Methods", "POST, GET, PUT")
+  next();
 });
 
-app.listen(app.get('port'), function() {
-  console.log('Node app is running on port', app.get('port'));
+app.put('/crear/:id', function( req,res ) {
+    contadores[req.params.id] = 0;
+    res.send( { creado: req.params.id, valor: contadores[req.params.id]} );
 });
 
+app.get('/contador/:id', function (req, res) {
+    res.send( "{ "+req.params.id+": "+ contadores[req.params.id] + "}" );
+});
 
+app.post('/sumar/:id', function (req, res) {
+    contadores[req.params.id]++;
+    res.send( "{ "+req.params.id+": "+ contadores[req.params.id] + "}" );
+});
+
+app.post('/borrar/:id', function (req, res) {
+    contadores[req.params.id] = 0;
+    res.send( "{ "+req.params.id+": "+ contadores[req.params.id] + "}" );
+});
+
+app.listen(puerto);
+console.log('Server running at http://127.0.0.1:'+puerto+'/');
